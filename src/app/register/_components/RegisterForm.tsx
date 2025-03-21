32793d2ad03276cd52e8d637e31450a2f346ac62
PureunKang <pureun0000@gmail.com>
2025-03-21 23:59:59 +0900

    feat : 회원가입 수파베이스 삽입 확인

---
+++ src/app/register/_components/RegisterForm.tsx
@@ -0,0 +1,132 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useRouter } from 'next/navigation';
+import supabase from '@/services/supabase';
+// import useUserStore from '@/store/useUserStore';
+
+export default function RegisterForm() {
+  //   const { setUserData } = useUserStore();
+
+  const formSchema = z
+    .object({
+      email: z.string().email({ message: '유효한 이메일을 입력해 주세요.' }),
+      password: z.string().min(8, {
+        message: '비밀번호는 8자 이상 입력해 주세요.',
+      }),
+      passwordConfirm: z.string().min(8, { message: '비밀번호를 다시 입력해 주세요.' }),
+      nickname: z.string().min(2, { message: '닉네임은 최소 2자 이상 입력해 주세요.' }),
+    })
+    .refine((data) => data.password === data.passwordConfirm, {
+      message: '비밀번호가 일치하지 않습니다.',
+      path: ['passwordConfirm'],
+    });
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+      passwordConfirm: '',
+      nickname: '',
+    },
+  });
+
+  const router = useRouter();
+
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
+    try {
+      const { email, password, nickname } = data;
+
+      const { data: registerData, error } = await supabase.auth.signUp({
+        email,
+        password,
+        options: {
+          data: { nickname },
+        },
+      });
+
+      if (error) {
+        throw new Error(error.message || '회원가입 실패');
+      }
+
+      //   // 가입 후 zustand 저장 로직
+      //   setUserData(registerData);
+      alert('회원가입이 완료되었습니다.');
+      router.push('/login');
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>이메일</FormLabel>
+              <FormControl>
+                <Input placeholder="example@exam.com" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="nickname"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>닉네임</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>비밀번호</FormLabel>
+              <FormControl>
+                <Input {...field} type="password" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="passwordConfirm"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>비밀번호 확인</FormLabel>
+              <FormControl>
+                <Input {...field} type="password" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">가입하기</Button>
+      </form>
+    </Form>
+  );
+}
---
+++ src/store/useUserStore.ts
@@ -0,0 +1,15 @@
+import { create } from 'zustand';
+import { immer } from 'zustand/middleware/immer';
+import { persist } from 'zustand/middleware';
+
+const useUserStore = create(
+  persist(
+    immer((set) => ({
+      userData: null,
+      setUserData: (data) => set(() => ({ userData: data })),
+    })),
+    { name: 'user-data' },
+  ),
+);
+
+export default useUserStore;
