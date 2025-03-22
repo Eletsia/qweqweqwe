1e64fcad985756e50511e95ffdafaaf54ec87bbb
33hyun <lggx12345@gmail.com>
2025-03-22 15:43:01 +0900

    feat: 공통헤더 컴포넌트 유저 데이터 하드코딩부분 스토어에서 로그인 유저정보와 로그아웃 함수 받아와 적용

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,35 +2,25 @@
 
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import useAuthStore from '@/store/authStore';
 
 export const Header = () => {
   const router = useRouter();
-  const [user, setUser] = useState<{ uid: string; nickname: string } | null>(null);
+  const { user, logout } = useAuthStore();
 
-  useEffect(() => {
-    // 하드코딩된 사용자 데이터 (로그인 구현 전까지 테스트 용도입니다!)
-    const userData = { uid: '12345', nickname: '테스트유저' };
-    setUser(userData);
-  }, []);
-
-  // 로그아웃 핸들러
   const handleLogout = () => {
-    setUser(null);
+    logout();
     router.push('/');
   };
 
   return (
     <header className="flex justify-between items-center px-4 py-2 border-b">
-      {/* 로고 클릭시 홈으로 이동 */}
       <Link href="/">
         <img src="/images/logo.svg" alt="Pick n Click Logo" className="h-10" />
       </Link>
 
-      {/* 네브 바 */}
       <nav className="flex items-center gap-4 text-sm">
         <Link href="/cart">장바구니</Link>
-        {/* 조건부 렌더링(로그인:로그아웃) */}
         {user ? (
           <>
             <Link href="/mypage">마이페이지</Link>
