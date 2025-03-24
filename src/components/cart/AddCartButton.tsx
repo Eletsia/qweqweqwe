e706c0230a5b41cc8fff05e8fd5b86689f5f5a1b
sohxxny <sohxxny@gmail.com>
2025-03-24 23:39:13 +0900

    feat: 상품 주문하기 버튼 구현

--- src/components/cart/AddCartButton.tsx
+++ src/components/cart/AddCartButton.tsx
@@ -27,6 +27,7 @@ export const AddCartButton = (item: CartItem) => {
       alert('동일한 상품이 장바구니에 있어 수량이 변경됩니다.');
       updateItem(item);
     } else {
+      alert('상품이 장바구니에 추가되었습니다.');
       addItem(item);
     }
   };
