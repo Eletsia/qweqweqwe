63d34a9c2621869a0b2c25a2bb074221ca9d5940
PureunKang <pureun0000@gmail.com>
2025-03-25 13:42:39 +0900

    chore : 안쓰는요소 삭제

--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -1,14 +1,7 @@
 'use client';
 
 import { Button } from '@/components/ui/button';
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from '@/components/ui/form';
+import { Form, FormControl, FormField, FormItem, FormLabel } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { z } from 'zod';
 import { useForm } from 'react-hook-form';
