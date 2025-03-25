6b7c1e9dcc04a374d429e8a0cddc5667398d8c45
UrePu <darkin31313@gmail.com>
2025-03-25 14:28:13 +0900

    config 수정

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   images: {
     domains: ['images.unsplash.com', 'picsum.photos', 'svqahkrytqcdumiejdmf.supabase.co'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'svqahkrytqcdumiejdmf.supabase.co',
+        pathname: '/storage/v1/object/public/item-img/uploads/**',
+      },
+    ],
   },
 };
 
