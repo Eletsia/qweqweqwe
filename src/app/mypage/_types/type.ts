259d118485531df4c373cd5e33ecf93765c68003
Eletsia <minjh2749@gmail.com>
2025-03-25 13:12:00 +0900

    add : type.ts

---
+++ src/app/mypage/_types/type.ts
@@ -0,0 +1,34 @@
+export type User = {
+  nickname: string;
+  email: string;
+  introduction: string;
+};
+
+export type TabContents = {
+  orders: Orders[];
+  reviews_written: Reviews[];
+  reviews_unwritten: Reviews[];
+};
+
+export type Orders = {
+  amount: number;
+  created_at: string;
+  item_id: number;
+  items: Items[];
+  order_id: number;
+  order_status: string;
+};
+
+export type Items = {
+  thumbnail: string;
+  title: string;
+};
+
+export type Reviews = {
+  content: string;
+  created_at: string;
+  item_id: number;
+  review_id: number;
+  uid: string;
+  written: boolean;
+};
