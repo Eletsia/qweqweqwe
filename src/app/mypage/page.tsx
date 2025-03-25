ac50a2f064f1b1f7afc9f78c93b988b61d31b47f
Eletsia <minjh2749@gmail.com>
2025-03-25 20:33:23 +0900

    bugfix : typeError

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable */
 'use client';
 import { getUserInfo } from '@/api/usersApi';
 import { useEffect, useState } from 'react';
@@ -7,7 +6,7 @@ import { getOrderedItemsByBuyerId } from '@/api/orderedItemsApi';
 import { getUnWrittenReviewByUserId, getWrittenReviewByUserId } from '@/api/reviewsApi';
 import { Profile } from './_components/Profile';
 import { StoreButton } from './_components/StoreButton';
-import { Orders, Reviews, TabContents, User } from './_types/type';
+import { Orders, Orders2, Reviews, TabContents, User } from './_types/type';
 import useAuthStore from '@/store/authStore';
 
 export default function MyPage() {
@@ -27,6 +26,12 @@ export default function MyPage() {
     reviews_written: [],
     reviews_unwritten: [],
   });
+  const convertOrders2ToOrders = (orders2: Orders2[]): Orders[] => {
+    return orders2.map((order) => ({
+      ...order,
+      items: order.items.length === 1 ? order.items[0] : order.items[0], // Items 배열에서 첫 번째 항목만 선택
+    }));
+  };
 
   useEffect(() => {
     async function fetchUsers() {
@@ -43,21 +48,22 @@ export default function MyPage() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const orders: Orders[] = (await getOrderedItemsByBuyerId(userId)) || [];
+        const orders2: Orders2[] = (await getOrderedItemsByBuyerId(userId)) || [];
+        const orders: Orders[] = convertOrders2ToOrders(orders2);
         const reviewsWritten: Reviews[] = (await getWrittenReviewByUserId(userId)) || [];
         const reviewsUnwritten: Reviews[] = (await getUnWrittenReviewByUserId(userId)) || [];
 
         setTabContents({
           orders: orders,
-          reviews_written: reviewsWritten || [],
-          reviews_unwritten: reviewsUnwritten || [],
+          reviews_written: reviewsWritten,
+          reviews_unwritten: reviewsUnwritten,
         });
       } catch (error) {
         console.error('Unexpected error', error);
       }
     };
     fetchData();
-  }, []);
+  }, [userId]);
 
   return (
     <div className="relative mx-auto mt-20 flex w-full max-w-4xl flex-col items-center p-6">
