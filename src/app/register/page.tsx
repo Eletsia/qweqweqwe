5aca635b4a88d8f085a01041f8a19714578d6303
PureunKang <pureun0000@gmail.com>
2025-03-27 12:01:37 +0900

    style : 회원가입 디자인 수정

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -3,7 +3,7 @@ import RegisterForm from '@/components/register/RegisterForm';
 export default function SignUpPage() {
   return (
     <>
-      <div className="mx-auto max-w-md p-4">
+      <div className="mx-auto mt-20 max-w-md p-4">
         <h1 className="mb-4 text-center text-2xl font-bold">회원가입</h1>
         <RegisterForm />
       </div>
